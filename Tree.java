4d91c6f853b393186f21707f3b5e978908366c5a
HHarrell <hharrell@hey-kiddo.com>
2023-10-03 16:16:48 -0400

    Node class skeleton

--- Tree.java
+++ Tree.java
@@ -3,5 +3,28 @@ import java.util.*;
 public class Tree {
     class Node {
 
+        String type;
+        String operator;
+        int value;
+
+        Node leftChild;
+        Node rightChild;
+
+        Node(String symbol) {
+            try {
+                int i = Integer.parseInt(symbol);
+                // value is an Integer
+                this.type = "constant";
+                this.value = i;
+
+            } catch (NumberFormatException e) {
+                this.type = "operator";
+                this.operator = symbol;
+            }
+        }
+
+        public String toString() {
+            return "node has a value " + value;
+        }
     }
 }
