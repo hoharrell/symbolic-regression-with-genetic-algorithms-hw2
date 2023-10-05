f3a176b308c1579a62475b788f93d7f511b55db7
cocross1 <112398487+cocross1@users.noreply.github.com>
2023-10-05 16:18:27 -0400

    Update Main.java
    Added the conversion of the csv file to a 2d array list of strings

--- Main.java
+++ Main.java
@@ -1,8 +1,32 @@
+import java.io.BufferedReader;  
+import java.io.FileReader;  
+import java.io.IOException;  
 import java.util.*;
-
-public class Main {
-    // for testing
-    public static void main(String[] args) {
-
-    }
+public class Main
+{  
+public static void main(String[] args)   
+{  
+String newLine = "";  
+String comma = ",";  
+ArrayList<ArrayList<String>> arr = new ArrayList<ArrayList<String>>();  
+try   
+{  
+BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\clcco\\Downloads\\HW2\\dataset1.csv"));
+while ((newLine = br.readLine()) != null)   //returns a Boolean value  
+{
+    ArrayList<String> subArr = new ArrayList<String>();
+    arr.add(subArr);
+String[] employee = newLine.split(comma);    // use comma as separator
+for(String s : employee){
+    subArr.add(s);
 }
+}  
+
+}   
+catch (IOException e)   
+{  
+e.printStackTrace();  
+}  
+}
+
+}  
