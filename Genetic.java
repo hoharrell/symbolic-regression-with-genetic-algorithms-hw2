6ce75eb4134ea538f85186f81c9a96c8e0ed3c50
cocross1 <112398487+cocross1@users.noreply.github.com>
2023-10-17 15:20:41 -0400

    Update Genetic.java
    begin building class

--- Genetic.java
+++ Genetic.java
@@ -2,6 +2,20 @@ import java.util.*;
 
 public class Genetic{
 
+    private ArrayList<ArrayList<String>> dataSet;
+    private int size;
 
+    public Genetic(ArrayList<ArrayList<String>> data, int populationSize)
+    {
+        dataSet = data;
+        size = populationSize;
+        //create some initial population of trees, size populationSize
+
+    }
+
+    public geneticAlgorithm()
+    {
+        
+    }
 
 }
