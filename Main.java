28307913450402144f5b070199bddbbd335dd945
cocross1 <112398487+cocross1@users.noreply.github.com>
2023-10-19 16:40:14 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,32 +1,43 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.BufferedReader;  
+import java.io.FileReader;  
+import java.io.IOException;  
 import java.util.*;
+public class Main
+{  
+public static void main(String[] args)   
+{  
+// String newLine = "";  
+// String comma = ",";  
+// ArrayList<ArrayList<String>> arr = new ArrayList<ArrayList<String>>();  
+// try   
+// {  
+// BufferedReader br = new BufferedReader(new FileReader("dataset1.csv"));
+// while ((newLine = br.readLine()) != null)   //returns a Boolean value  
+// {
+//     ArrayList<String> subArr = new ArrayList<String>();
+//     arr.add(subArr);
+// String[]  vals = newLine.split(comma);    // use comma as separator
+// for(String s : vals){
+//     subArr.add(s);
+// }
+// }  
 
-public class Main {
-    double MUTATION_PROB = 0.1;
-    double CROSSOVER_PROB = 0.6;
+// }   
+// catch (IOException e)   
+// {  
+// e.printStackTrace();  
+// }  
 
-    public static void main(String[] args) {
-        String newLine = "";
-        String comma = ",";
-        ArrayList<ArrayList<String>> arr = new ArrayList<ArrayList<String>>();
-        try {
-            BufferedReader br = new BufferedReader(new FileReader("dataset1.csv"));
-            while ((newLine = br.readLine()) != null) // returns a Boolean value
-            {
-                ArrayList<String> subArr = new ArrayList<String>();
-                arr.add(subArr);
-                String[] vals = newLine.split(comma); // use comma as separator
-                for (String s : vals) {
-                    subArr.add(s);
-                }
-            }
+Tree tree1 = new Tree(2,false);
+Tree tree2 = new Tree(2, false);
+System.out.println(tree1.inOrderTraverse());
+System.out.println(tree2.inOrderTraverse());
+Tree[] arr = Tree.crossover(tree1,tree2);
+System.out.println(tree1.inOrderTraverse());
+System.out.println(tree2.inOrderTraverse());
+System.out.println(arr[0].inOrderTraverse());
+System.out.println(arr[1].inOrderTraverse());
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // generate two random Trees
-    }
 }
+
+}  
