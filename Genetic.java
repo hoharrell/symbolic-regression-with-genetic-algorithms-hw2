ddacdde9c4e05d80dbe013e7445359c52a9dd0e6
cocross1 <112398487+cocross1@users.noreply.github.com>
2023-10-22 13:45:02 -0400

    Update Genetic.java
    Added method to perform genetic algorithm itself and method to perform tournament selection.

--- Genetic.java
+++ Genetic.java
@@ -4,12 +4,93 @@ import java.util.*;
 //
 public class Genetic{
 
+    public static final int DEPTH = 4;
+    public static final int GENERATIONS = 5;
 
-    public static void geneticAlgorithm(ArrayList<ArrayList<String>> data, int populationSize)
+    public static Tree geneticAlgorithm(ArrayList<ArrayList<String>> data, int populationSize)
     {
 
-        //create some initial population of trees, size populationSize
+        ArrayList<Tree> parents = new ArrayList<Tree>();
+        Tree temp;
+        for(int i=0; i<populationSize; i++)
+        {
+            temp = new Tree(DEPTH, false);
+            parents.add(temp);
+            temp.fitness =  evaluate(data, temp, data.size()/4);
+        }
+        ArrayList<Tree> children;
+        temp = new Tree(DEPTH, false);
+        for(int i=0; i<GENERATIONS; i++ )
+        {
+            children = new ArrayList<Tree>();
 
+            while(children.size() < parents.size()){
+                System.out.println("Children: " + children.size() + " and parents: " + parents.size());
+                Tree[] arr = tournamentSelect(parents, (int)(.2*populationSize), data);
+                arr = Tree.crossover(arr[0],arr[1]);
+                Tree child = arr[(int)(2*Math.random())];
+                if(Math.random() > .95)
+                    child.mutate();
+                
+                if(child.fitness > temp.fitness)
+                    temp = child;
+                
+                children.add(child);
+            }
+            System.out.println(i);
+            parents = children;
+            
+        }
+        return temp;
+
+    }
+
+
+    public static Tree[] tournamentSelect(ArrayList<Tree> trees, int tournamentSize, ArrayList<ArrayList<String>> data)
+    {
+        Tree[] arr = new Tree[2];
+        Set<Tree> players = new HashSet<Tree>();
+        Tree fittest = trees.get(0);
+        Tree temp;
+        int fitness = 0;
+        double upset = .98;
+
+        while(players.size()<tournamentSize)
+        {
+            temp = trees.get((int)(Math.random()*trees.size()));
+            players.add(temp);
+            if(temp.fitness > fitness){
+                fittest = temp;
+            }
+            if(Math.random() > upset)
+            {
+                fittest = temp;
+                break;
+            }
+        }
+        trees.remove(fittest);
+        arr[0] = fittest;
+        players = new HashSet<Tree>();
+        fittest = trees.get(0);
+        fitness = 0;
+
+        while(players.size()<tournamentSize)
+        {
+            temp = trees.get((int)(Math.random()*trees.size()));
+            players.add(temp);
+            if(temp.fitness > fitness){
+                fittest = temp;
+            }
+            if(Math.random() > upset)
+            {
+                fittest = temp;
+                break;
+            }
+        }
+        arr[1] = fittest;
+        trees.add(arr[0]);
+
+        return arr;
     }
 
     /*
@@ -25,8 +106,8 @@ public class Genetic{
         double error = 0;
         for(int i=1; i<=cap; i++)
         {
-            actual = Integer.parseInt(data.get(i).get(1));
-            guess = tree.expressionResult(Integer.parseInt(data.get(i).get(0)));
+            actual = Double.parseDouble(data.get(i).get(1));
+            guess = tree.expressionResult(Double.parseDouble(data.get(i).get(0)));
             error+=((actual-guess)*(actual-guess));
 
         }
