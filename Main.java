3ab09a55aaba614558b067819e8cb20c1fa2a9e8
cocross1 <112398487+cocross1@users.noreply.github.com>
2023-10-22 18:09:42 -0400

    Update Main.java
    now to test the genetic algorithm

--- Main.java
+++ Main.java
@@ -1,42 +1,51 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.BufferedReader;  
+import java.io.FileReader;  
+import java.io.IOException;  
 import java.util.*;
-
-public class Main {
-    public static void main(String[] args) {
-        // String newLine = "";
-        // String comma = ",";
-        // ArrayList<ArrayList<String>> arr = new ArrayList<ArrayList<String>>();
-        // try
-        // {
-        // BufferedReader br = new BufferedReader(new FileReader("dataset1.csv"));
-        // while ((newLine = br.readLine()) != null) //returns a Boolean value
-        // {
-        // ArrayList<String> subArr = new ArrayList<String>();
-        // arr.add(subArr);
-        // String[] vals = newLine.split(comma); // use comma as separator
-        // for(String s : vals){
-        // subArr.add(s);
-        // }
-        // }
-
-        // }
-        // catch (IOException e)
-        // {
-        // e.printStackTrace();
-        // }
-
-        Tree tree1 = new Tree(2, false);
-        Tree tree2 = new Tree(2, false);
-        System.out.println(tree1.inOrderTraverse());
-        System.out.println(tree2.inOrderTraverse());
-        Tree mutatedTree = Tree.mutate(tree1);
-        System.out.println(mutatedTree.inOrderTraverse());
-        Tree[] arr = Tree.crossover(tree1, tree2);
-        System.out.println(arr[0].inOrderTraverse());
-        System.out.println(arr[1].inOrderTraverse());
-
+public class Main
+{  
+public static void main(String[] args)   
+{  
+    String newLine = "";  
+    String comma = ",";  
+    ArrayList<ArrayList<String>> arr = new ArrayList<ArrayList<String>>();  
+    try   
+    {  
+    BufferedReader br = new BufferedReader(new FileReader("dataset1.csv"));
+    while ((newLine = br.readLine()) != null)   //returns a Boolean value  
+    {
+        ArrayList<String> subArr = new ArrayList<String>();
+        arr.add(subArr);
+    String[]  vals = newLine.split(comma);    // use comma as separator
+    for(String s : vals){
+        subArr.add(s);
     }
+    }
+    Tree regression = Genetic.geneticAlgorithm(arr);  
+    System.out.println(regression.inOrderTraverse());
 
-}
+    }   
+    catch (IOException e)   
+    {  
+    e.printStackTrace();  
+    }  
+
+    // Tree tree1 = new Tree(4, false);
+    // Tree tree2 = new Tree(4, false);
+    // System.out.println("Tree 1:" + tree1.inOrderTraverse());
+    // System.out.println("Tree 2: " + tree2.inOrderTraverse());
+    // tree1.simplify();
+    // tree2.simplify();
+    // Tree mutatedTree = tree1.mutate();
+    // System.out.println(mutatedTree.inOrderTraverse());
+    // Tree[] arr = Tree.crossover(tree1, tree2);
+    // System.out.println("Simplified Tree 1:" + tree1.inOrderTraverse());
+    // System.out.println("Simplified Tree 2: " + tree2.inOrderTraverse());
+    // System.out.println(arr[0].inOrderTraverse());
+    // System.out.println(arr[1].inOrderTraverse());
+
+
+ }
+
+
+} 
