5994cfa45d787f21e3404f73ef00fbebfc940313
cocross1 <112398487+cocross1@users.noreply.github.com>
2023-10-26 22:11:02 -0400

    Update Main.java
    Added some preprocessing stuff with interquartile range and scaling values

--- Main.java
+++ Main.java
@@ -12,7 +12,7 @@ public static void main(String[] args)
     ArrayList<ArrayList<String>> arr = new ArrayList<ArrayList<String>>();  
     try   
     {  
-    BufferedReader br = new BufferedReader(new FileReader("HW2-main\\dataset3.csv"));
+    BufferedReader br = new BufferedReader(new FileReader("HW2-main\\dataset2.csv"));
     while ((newLine = br.readLine()) != null)   //returns a Boolean value  
     {
         ArrayList<String> subArr = new ArrayList<String>();
@@ -22,9 +22,11 @@ public static void main(String[] args)
         subArr.add(s);
     }
     }
-    Tree regression = Genetic.geneticAlgorithm(arr, false,true);  
-    System.out.println(regression.inOrderTraverse());
+    arr.remove(0);
+    arr = preProcess(arr);
 
+    Tree regression = Genetic.geneticAlgorithm(arr, true,false);  
+    System.out.println(regression.inOrderTraverse());
     }   
     catch (IOException e)   
     {  
@@ -82,6 +84,75 @@ public static void main(String[] args)
     // System.out.println("Simplified Tree 2: " + tree2.inOrderTraverse());
     // System.out.println(arr[0].inOrderTraverse());
     // System.out.println(arr[1].inOrderTraverse());
+
+
+ }
+
+ public static class Data implements Comparable<Data>{
+    ArrayList<String> arr;
+
+    Data(ArrayList<String> arr)
+    {
+        this.arr = arr;
+    }
+
+    public int compareTo(Data other)
+    {
+        double one = Double.parseDouble(arr.get(3));
+        double two = Double.parseDouble(other.arr.get(3));
+        if(two-one < 0)
+            return -1;
+        if(two-one == 0)
+            return 0;
+
+        return 1;
+    }
+ }
+ 
+
+ public static ArrayList<ArrayList<String>> preProcess(ArrayList<ArrayList<String>> data)
+ {
+
+    Double i;
+    Double j;
+    for(ArrayList<String> arr : data)
+    {
+        i = Double.parseDouble(arr.get(0));
+        j = Double.parseDouble(arr.get(1));
+        i/=Math.pow(10,6);
+        j/=Math.pow(10,6);
+        arr.remove(0);
+        arr.remove(1);
+        arr.add(0,j.toString());
+        arr.add(0,i.toString());
+    }
+
+
+    PriorityQueue<Data> arr = new PriorityQueue<Data>();
+    for(ArrayList<String> arr2 : data)
+    {
+        arr.add(new Data(arr2));
+    }
+
+    ArrayList<ArrayList<String>> processed = new ArrayList<ArrayList<String>>();
+    while(!arr.isEmpty())
+    {
+        processed.add(arr.remove().arr);
+    }
+    ArrayList<ArrayList<String>> result = new ArrayList<ArrayList<String>>();
+    
+    for(int x=0; x<processed.size(); x++)
+    {
+        
+        if((x > processed.size()/4) && (x < 3*processed.size()/4)){
+            result.add(processed.get(x));
+
+        }
+    }
+
+    System.out.println(result.size());
+    return result;
+
 
 
  }
